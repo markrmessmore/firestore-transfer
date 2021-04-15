f20bff509dc1bbebbd2222230dbc4f5da75a6b73
Mark <markrmessmore@hotmail.com>
2021-04-15 16:52:27 -0400

    Added about page.

---
+++ src/components/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card-text>
+        <v-toolbar flat dense dark color="secondary">
+            <v-toolbar-title>About</v-toolbar-title>
+        </v-toolbar>
+        <v-row class="pa-3">
+            
+        </v-row>
+    </v-card-text>
+</template>
+
+<script>
+export default {
+    computed: {
+        
+    }
+}
+</script>
